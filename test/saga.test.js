78e854dce0bd4d52b111d8c13324f73070bf07e1
Nicholas Dujay <nickdujay@gmail.com>
2017-08-20 12:13:42 -0400

    add more tests

--- test/saga.test.js
+++ test/saga.test.js
@@ -16,7 +16,7 @@ test('the main saga is just `takeEvery`', () => {
   expect(gen.next()).toEqual({ done: true });
 });
 
-test('the `get` action is handled correctly', () => {
+test('the fetch saga works for a `get` action', () => {
   const body = { hello: 'world' };
   const response = new Response(JSON.stringify(body));
 
@@ -30,24 +30,73 @@ test('the `get` action is handled correctly', () => {
   const action = get('/v1', { success, fail: jest.fn() });
   const gen = fetchSaga(action);
 
-  // first, it should call with the right method
+  // first, it should wait for the result of the fetch
   expect(gen.next()).toEqual({
     done: false,
     value: call(fetch, '/v1', { method: 'GET', credentials: 'include' })
   });
 
-  // then, it should try and call the success handler
+  // then, once it gets the fetch result, it should wait for the next action
   expect(gen.next(response)).toEqual({
     done: false,
     value: call(Promise.resolve, success(response))
   });
 
-  // then, it should call the success action creator
+  // then, it should try and give back that action
   expect(gen.next(createHelloAction(body))).toEqual({
     done: false,
     value: put(createHelloAction(body))
   });
 
   // then, it should be done
+  expect(gen.next()).toEqual({ done: true });
+});
+
+test('the fetch saga allows users to call `response.text`', () => {
+  const text = 'i am some useless text :)';
+  const response = new Response(text);
+
+  const createHelloAction = payload => ({
+    type: 'HELLO',
+    payload,
+    error: null
+  });
+  const success = response => response.text().then(createHelloAction);
+
+  const action = get('/v1', { success, fail: jest.fn() });
+  const gen = fetchSaga(action);
+
+  // first yield (call, waiting for fetch response)
+  gen.next();
+  // second yield (got fetch response, waiting for next action)
+  gen.next(response);
+  // third yield, (got next action, should return it correctly)
+  expect(gen.next(createHelloAction(text))).toEqual({
+    done: false,
+    value: put(createHelloAction(text))
+  });
+});
+
+test('the fetch saga will call the fail action creator when it gets an error', () => {
+  const text = 'i am some useless text :)';
+  const response = new Response(text, { status: 400 });
+
+  const success = jest.fn();
+  const error = new Error('Failed http code.');
+  const fail = err => ({ type: 'ERROR', payload: error, error: true });
+
+  const action = get('/v1', { success, fail });
+  const gen = fetchSaga(action);
+
+  // first yield (call, waiting for fetch response)
+  gen.next();
+  // second yield (got fetch response, waiting for next action)
+  gen.next(response);
+  expect(success).toBeCalledWith(response);
+  // throw an error
+  expect(gen.throw(error)).toEqual({
+    done: false,
+    value: put(fail(error))
+  });
   expect(gen.next()).toEqual({ done: true });
 });
